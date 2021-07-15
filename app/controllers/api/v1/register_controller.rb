eac0bbab51878f9039e8cc00e6d38585cd014b14
Kris Litman <kris.d.litman@gmail.com>
2021-07-15 17:53:45 -0400

    Added method to registration cont to delete all users
    Co-authored by: 68141454+jordanfbeck0528@users.noreply.github.com

--- app/controllers/api/v1/register_controller.rb
+++ app/controllers/api/v1/register_controller.rb
@@ -1,21 +1,29 @@
 class Api::V1::RegisterController < ApplicationController
 
   def create
-    user = User.new(
-                    email: user_params[:email].downcase,
-                    password: user_params[:password],
-                    password_confirmation: params[:password_confirmation]
-                  )
-    if user.save
-      render json: UserSerializer.new(user), status: :created
+    if user_params[:email] == user_params[:verify_email]
+      user = User.new(
+                      email: user_params[:email].downcase,
+                      password: user_params[:password],
+                      password_confirmation: params[:password_confirmation]
+                    )
+      if user.save
+        render json: UserSerializer.new(user), status: :created
+      else
+        render json: {'message': {'error': "#{user.errors.full_messages}" }}, status: :bad_request
+      end
     else
       render json: {'message': {'error': "#{user.errors.full_messages}" }}, status: :bad_request
     end
   end
 
+  def destroy
+    User.destroy_all
+  end
+
   private
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :verify_email, :password, :password_confirmation)
   end
 end
